9f4a7f15fead853daa4d3fc8857045c0e754cd51
username1232-stack <royvelportezajr@gmail.com>
2026-09-13 23:00:17 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@ const mineflayer = require('mineflayer');
 const config = require('./config.json');
 
 const bot = mineflayer.createBot({
-  host: config.serverHost,
-  port: config.serverPort,
-  username: config.botUsername,
+  host: config.CalciumSMP.aternos.me,
+  port: config.50765,
+  username: config.royalbot,
   auth: 'offline',
   version: false,
   viewDistance: config.botChunk
